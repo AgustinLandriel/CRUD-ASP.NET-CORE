68b885d151e866965cf5fcd06857ada3bc476b64
Agustin Landriel <agusrojo20@hotmail.com>
2023-02-09 19:15:09 -0300

    Se creó la vista de Login

---
+++ CRUD-CORE-MVC/Controllers/SesionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_CORE_MVC.Controllers
+{
+	public class SesionController : Controller
+	{
+		public IActionResult Registro()
+		{
+			return View();
+		}
+	}
+}
---
+++ CRUD-CORE-MVC/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_CORE_MVC.Models
+{
+    public class UsuarioModel
+    {
+        public int idUsuario { get; set; }
+        public string User { get; set; }
+        public string Contraseña { get; set; }
+        public bool tipoUser { get; set; }
+    }
+}
